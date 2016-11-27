7c0f259b3683bd4cf1683fd6cc2dc39b183dce6e
songqun <songqun001@gmail.com>
2016-11-27 22:48:00 +0800

    level-13

---
+++ 13.py
@@ -0,0 +1,7 @@
+import xmlrpc.client
+
+server=xmlrpc.client.ServerProxy("http://www.pythonchallenge.com/pc/phonebook.php")
+print(server.system.listMethods())
+print(server.system.methodHelp("phone"))
+print(server.system.methodSignature("phone"))
+print(server.phone("Bert"))
